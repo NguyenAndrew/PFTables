4fdb40bcd5b4b255f9333339257e972686d96494
NguyenAndrew <anguyen29@wisc.edu>
2019-02-11 00:08:48 -0500

    Give the ability to use custom arrays instead of just using faker

--- README.md
+++ README.md
@@ -1,41 +1,55 @@
 # PFTables
 
-Generate CSV tables with fake data (in Python)
+Generate CSV tables with fake data (in Python). Can use both faker or custom arrays to generate fake data
 
-Installation Instructions:
+## Installation Instructions:
 
 1. Install Python 3 (Python 3.7.2)
 2. Install pipenv (pip install pipenv)
 3. Run the command: `pipenv shell`
 4. Run the command: `pipenv install --dev`
 
-Example Usage:
+## Example Usage
 
-1. Create input.csv (See https://faker.readthedocs.io/en/master/ for possible commands)
+1. Create input.csv (See https://faker.readthedocs.io/en/master/ to see available faker functions)
 
 ```
-"Company","Color"
-"company","color_name"
+"Company","Color", "Food"
+"company","color_name", "[Taco's | Burger""s | Pizza,s]"
 ```
 
-2. Run the command: `python python pftables.py 3`
+Company | Color | Food
+--- |--- | ---
+ company | color_name | [Taco's \| Burger"s \| Pizza,s]
+ 
+
+2. Run the command: `python python pftables.py 5`
 
 
 3. You should now see and be able to use output.csv
 
 ```
-"Company","Color"
-"Simon PLC","Sienna"
-"Trujillo-White","HotPink"
-"Harmon, Brown and Crawford","SlateGray"
+"Company","Color","Food"
+"Williams Group","LightSlateGray","Pizza,s"
+"Hebert and Sons","Snow","Taco's"
+"Ford, Cox and Riley","Lime","Taco's"
+"Evans Inc","Orchid","Burger""s"
+"Garcia, Rhodes and Willis","NavajoWhite","Pizza,s"
 
 ```
 
+Company | Color | Food
+--- | --- | ---
+Williams Group | LightSlateGray | Pizza,s
+Hebert and Sons | Snow | Taco's
+Ford, Cox and Riley | Lime | Taco's
+Evans Inc | Orchid |Burger"s
+Garcia, Rhodes and Willis | NavajoWhite | Pizza,s
 
 
-Dependencies
+## Dependencies
 
 Dependency | Reason for Using
 --- |---
- Pylint | Lints
+ Pylint | Linter
  Faker | Generates Fake Data
--- pftables.py
+++ pftables.py
@@ -1,8 +1,9 @@
 """ PFTables creates a fake table CSV file in Python """
-import sys
 import csv
-import os
 import errno
+import os
+import random
+import sys
 from faker import Faker
 FAKE = Faker()
 
@@ -13,41 +14,44 @@ def main():
         return
 
     csv_row_count = int(sys.argv[1])
-    csv_header, faker_executables = parse_input_csv()
+    csv_header, array_or_faker_executables = parse_input_csv()
 
+    print("Generating output.csv...")
     silent_remove('output.csv')
     with open('output.csv', 'w', newline='') as output_file:
         csv_writer = csv.writer(output_file, quoting=csv.QUOTE_ALL)
         csv_writer.writerow(csv_header)
-
         for _ in range(csv_row_count):
-            csv_writer.writerow(generate_fake_list(faker_executables))
+            csv_writer.writerow(generate_fake_list(array_or_faker_executables))
+    print("... and finished! Created output.csv with " + str(csv_row_count) + " fake data rows.")
 
 ### Helper Functions located below ###
 
 def parse_input_csv():
     """ Returns the headers and the faker executables"""
     with open('input.csv') as input_file:
-        csv_reader = csv.reader(input_file, delimiter=',')    
+        csv_reader = csv.reader(
+            input_file,
+            quotechar='"',
+            delimiter=',',
+            quoting=csv.QUOTE_ALL,
+            skipinitialspace=True)  
         csv_header = next(csv_reader)
-        faker_executables = [
-            'FAKE.' + function_name + '()'
+        array_or_faker_executables = [
+            make_array_or_faker_function_string(function_name)
             for function_name in next(csv_reader)
         ]
-        return [csv_header, faker_executables]
+        return [csv_header, array_or_faker_executables]
 
-def generate_fake_list(faker_executables):
-    """ Returns an array with fake data """
-    return [
-        exec_and_return(executable) for executable in faker_executables
-    ]
+def make_array_or_faker_function_string(cell):
+    """
+    Either selects an array element at random, or
+    create the faker function string, depending on the input format
+    """
+    if (cell.startswith('[') and cell.endswith(']')):
+        return cell[1:-1].split(' | ') # Removes the brackets before split
 
-def exec_and_return(executable):
-    """ Uses Python's exec command, and obtains its return value """
-    # To understand this code: https://bugs.python.org/issue4831
-    fresh_locals = {}
-    exec('return_value = ' + executable, globals(), fresh_locals)
-    return fresh_locals['return_value']
+    return 'FAKE.' + cell + '()'
 
 def silent_remove(filename):
     """ Remove the file silently if exists, for all scenarios toss an error """
@@ -56,6 +60,29 @@ def silent_remove(filename):
     except OSError as error:
         if error.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
             raise # re-raise exception if a different error occurred
+
+def generate_fake_list(faker_executables):
+    """ Returns an array with fake data """
+    return [
+        select_array_or_exec_function(executable) for executable in faker_executables
+    ]
+
+def select_array_or_exec_function(cell):
+    """
+    Either selects an array element at random, or
+    executes the function, depending on the input format
+    """
+    if isinstance(cell, list):
+        return random.choice(cell)
+
+    return exec_and_return(cell)
+
+def exec_and_return(executable):
+    """ Uses Python's exec command, and obtains its return value """
+    # To understand this code: https://bugs.python.org/issue4831
+    fresh_locals = {}
+    exec('return_value = ' + executable, globals(), fresh_locals)
+    return fresh_locals['return_value']
 
 ### Execute the Main Function ###
 if __name__ == '__main__':
